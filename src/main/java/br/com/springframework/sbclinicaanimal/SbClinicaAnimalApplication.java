2c25b0a6af288b09b67d23ebd8937591beae50cb
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2018-12-09 17:53:22 -0200

    Initial project commit

---
+++ src/main/java/br/com/springframework/sbclinicaanimal/SbClinicaAnimalApplication.java
@@ -0,0 +1,12 @@
+package br.com.springframework.sbclinicaanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbClinicaAnimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbClinicaAnimalApplication.class, args);
+    }
+}
